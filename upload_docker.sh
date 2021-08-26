a8ab8a1d74bd043289b8495722f3f6554568f8b1
Deyaa M <deyaaabolata@gmail.com>
2021-08-26 10:33:41 +0200

    auth to ecr and push

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=135834287615.dkr.ecr.eu-west-2.amazonaws.com
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${dockerpath}
+docker tag flask:latest ${dockerpath}/flask:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}/flask:latest
