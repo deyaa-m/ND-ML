a220695a2d56e6b6924fb861f27c7522789c8f59
Deyaa M <deyaaabolata@gmail.com>
2021-08-26 16:53:27 +0200

    flask app deploy & svc file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,13 +9,16 @@ dockerpath=135834287615.dkr.ecr.eu-west-2.amazonaws.com
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-docker stack deploy -n flask-ns --compose-file docker-compose.yml flask-stack
+minikube start
+#eval $(minikube docker-env)
+docker pull ${dockerpath}/flask:latest
+kubectl apply -f flask-k8s.yml
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods -n flask-ns
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
-#kubectl port-forward service/flask 80:8000
+kubectl port-forward service/flask-service 8000:8000
 
